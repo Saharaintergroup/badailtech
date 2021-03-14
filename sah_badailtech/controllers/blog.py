06ebeb8bd5f2d3eaf58e250c7c409b623d703672
ayah <ayahashraf236@gmail.com>
2021-03-15 01:51:59 +0200

    [MOD] sah_badailtech: Event and Event details pages

---
+++ sah_badailtech/controllers/blog.py
@@ -0,0 +1,129 @@
+import json
+
+from odoo import http, fields
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.website.controllers.main import QueryURL
+from odoo.http import request
+from odoo.tools import sql, werkzeug, get_lang, pytz, babel, OrderedDict, itertools
+
+
+
+# from odoo.addons.website_blog.controllers
+
+
+class InheritBlog(http.Controller):
+
+    def nav_list(self, blog=None):
+        dom = blog and [('blog_id', '=', blog.id)] or []
+        if not request.env.user.has_group('website.group_website_designer'):
+            dom += [('post_date', '<=', fields.Datetime.now())]
+        groups = request.env['blog.post']._read_group_raw(
+            dom,
+            ['name', 'post_date'],
+            groupby=["post_date"], orderby="post_date desc")
+        for group in groups:
+            (r, label) = group['post_date']
+            start, end = r.split('/')
+            group['post_date'] = label
+            group['date_begin'] = start
+            group['date_end'] = end
+
+            locale = get_lang(request.env).code
+            start = pytz.UTC.localize(fields.Datetime.from_string(start))
+            tzinfo = pytz.timezone(request.context.get('tz', 'utc') or 'utc')
+
+            group['month'] = babel.dates.format_datetime(start, format='MMMM', tzinfo=tzinfo, locale=locale)
+            group['year'] = babel.dates.format_datetime(start, format='yyyy', tzinfo=tzinfo, locale=locale)
+
+        return OrderedDict((year, [m for m in months]) for year, months in itertools.groupby(groups, lambda g: g['year']))
+
+    @http.route([
+        '''/blog/<model("blog.blog"):blog>/<model("blog.post", "[('blog_id','=',blog.id)]"):blog_post>''',
+    ], type='http', auth="public", website=True, sitemap=True)
+    def blog_post(self, blog, blog_post, tag_id=None, page=1, enable_editor=None, **post):
+        """ Prepare all values to display the blog.
+
+        :return dict values: values for the templates, containing
+
+         - 'blog_post': browse of the current post
+         - 'blog': browse of the current blog
+         - 'blogs': list of browse records of blogs
+         - 'tag': current tag, if tag_id in parameters
+         - 'tags': all tags, for tag-based navigation
+         - 'pager': a pager on the comments
+         - 'nav_list': a dict [year][month] for archives navigation
+         - 'next_post': next blog post, to direct the user towards the next interesting post
+        """
+
+        if not blog.can_access_from_current_website():
+            raise werkzeug.exceptions.NotFound()
+
+        BlogPost = request.env['blog.post']
+        date_begin, date_end = post.get('date_begin'), post.get('date_end')
+
+
+
+
+        domain = request.website.website_domain()
+        blogs = blog.search(domain, order="create_date, id asc")
+
+        tag = None
+        if tag_id:
+            tag = request.env['blog.tag'].browse(int(tag_id))
+        blog_url = QueryURL('', ['blog', 'tag'], blog=blog_post.blog_id, tag=tag, date_begin=date_begin,
+                            date_end=date_end)
+
+        if not blog_post.blog_id.id == blog.id:
+            return request.redirect("/blog/%s/%s" % (slug(blog_post.blog_id), slug(blog_post)), code=301)
+
+        tags = request.env['blog.tag'].search([])
+
+        # Find next Post
+        blog_post_domain = [('blog_id', '=', blog.id)]
+        if not request.env.user.has_group('website.group_website_designer'):
+            blog_post_domain += [('post_date', '<=', fields.Datetime.now())]
+
+        all_post = BlogPost.search(blog_post_domain)
+
+        if blog_post not in all_post:
+            return request.redirect("/blog/%s" % (slug(blog_post.blog_id)))
+
+        # should always return at least the current post
+        all_post_ids = all_post.ids
+        current_blog_post_index = all_post_ids.index(blog_post.id)
+        nb_posts = len(all_post_ids)
+        next_post_id = all_post_ids[(current_blog_post_index + 1) % nb_posts] if nb_posts > 1 else None
+        next_post = next_post_id and BlogPost.browse(next_post_id) or False
+
+        similar_posts = []
+        for blog_id in BlogPost.search([]):
+            for tag in blog_post.tag_ids:
+                if tag in blog_id.tag_ids and blog_id != blog_post:
+                    similar_posts.append(blog_id)
+
+
+        values = {
+            'tags': tags,
+            'tag': tag,
+            'blog': blog,
+            'blog_post': blog_post,
+            'blogs': blogs,
+            'main_object': blog_post,
+            'nav_list': self.nav_list(blog),
+            'enable_editor': enable_editor,
+            'similar_posts': similar_posts,
+            'next_post': next_post,
+            'date': date_begin,
+            'blog_url': blog_url,
+        }
+
+
+        response = request.render("website_blog.blog_post_complete", values)
+
+        if blog_post.id not in request.session.get('posts_viewed', []):
+            if sql.increment_field_skiplock(blog_post, 'visits'):
+                if not request.session.get('posts_viewed'):
+                    request.session['posts_viewed'] = []
+                request.session['posts_viewed'].append(blog_post.id)
+                request.session.modified = True
+        return response
