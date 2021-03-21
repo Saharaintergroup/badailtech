4b313a04e831060c3f937c1dd80b12c5c9c2aac9
ayah <ayahashraf236@gmail.com>
2021-03-21 02:53:46 +0200

    [MOD] sah_badailtech:home page tab system and news page similar blogs

--- sah_badailtech/static/src/js/script.js
+++ sah_badailtech/static/src/js/script.js
@@ -5,7 +5,7 @@ $(function () {
         margin:5,
         nav:true,
         center:true,
-        autoplay:true,
+        autosahay:true,
         responsive:{
             0: {
                 items: 1,
@@ -24,9 +24,9 @@ $(function () {
     $('.odoo_services_slider').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
+        nav:false,
         center:true,
-        autoplay:true,
+        autosahay:true,
         responsive:{
             768: {
                 items: 1,
@@ -39,18 +39,29 @@ $(function () {
     $('.odoo_blog_details_slider').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
-        center:true,
-        autoplay:true,
+        nav:false,
+        center:false,
+        autosahay:true,
         responsive:{
             768: {
                 items: 1,
             },
             1024: {
-                items:2
+                items:1.5
             }
         }
     });
 
 });
+function tabEventNews(e,imgName) {
+    $('.tab-head-item, .tabs_event_news').removeClass("active");
+    $(e).addClass("active");
+
+    $('.sah-tab, .tab-content-item').hide();
+    $('#'+imgName).fadeIn();
+
+    $('.sah-tab, .tab-content-item').removeClass("active");
+    $('#'+imgName).addClass("active");
+    return false;
+}
 //$owl.trigger('refresh.owl.carousel');
